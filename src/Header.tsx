0a21d8eacc13b53d082a94e89f5c255d21d94e28
Ali Rıza Altıparmak <5054445+araltiparmak@users.noreply.github.com>
2025-02-03 18:33:35 +0100

    add welcome text (#24)

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,4 @@
-import { aboutText, links, name, title } from "./data/DB.ts";
+import { links, name, title, welcomeText } from "./data/header.ts";
 import { Link } from "./components/atoms/Link.tsx";
 
 export const Header = () => {
@@ -6,30 +6,27 @@ export const Header = () => {
     <header className="bg-slate-50/50 backdrop-blur-sm px-6 py-8 sticky top-0 z-50">
       <div className="max-w-4xl mx-auto">
         <div className="flex items-center justify-between">
-          <div>
-            <h1 className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 to-purple-600">
-              {name}
-            </h1>
-            <p className="text-[13px] text-gray-500/90 tracking-wide mt-0.5 font-light">
-              {title}
-            </p>
-          </div>
+          <NameTitle />
 
           <SocialLinks />
         </div>
 
-        <div className="mt-4 pt-4 border-t border-gray-200/80">
-          {aboutText.map((text, index) => (
-            <p
-              key={index}
-              className="text-[14px] text-gray-600 mt-1.5 first:mt-0"
-            >
-              {text}
-            </p>
-          ))}
-        </div>
+        <Welcome />
       </div>
     </header>
+  );
+};
+
+const NameTitle = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 to-purple-600">
+        {name}
+      </h1>
+      <p className="text-[13px] text-gray-500/90 tracking-wide mt-0.5 font-light">
+        {title}
+      </p>
+    </div>
   );
 };
 
@@ -37,13 +34,16 @@ const SocialLinks = () => {
   return (
     <div className="flex items-center gap-4">
       {links.map((link) => (
-        <Link
-          key={link.url}
-          url={link.url}
-          label={link.label}
-          icon={link.icon}
-        />
+        <Link url={link.url} label={link.label} icon={link.icon} />
       ))}
+    </div>
+  );
+};
+
+const Welcome = () => {
+  return (
+    <div className="mt-4 pt-4 border-t border-gray-200/80">
+      <div className="mb-4">{welcomeText}</div>
     </div>
   );
 };
--- src/data/DB.ts
+++ src/data/DB.ts
@@ -1,33 +1,6 @@
-//Header
-export type Link = {
-  url: string;
-  label: string;
-  icon?: string;
-};
-export const name = "Ali Rıza Altıparmak";
-export const title = "Senior Software Engineer";
-export const links: Link[] = [
-  {
-    label: "LinkedIn",
-    url: "https://www.linkedin.com/in/araltiparmak/",
-    icon: "linkedin.png",
-  },
-  {
-    label: "GitHub",
-    url: "https://github.com/araltiparmak/",
-    icon: "github.svg",
-  },
-];
-
 //Footer
 const buildWith = "React & Tailwind CSS";
 export const footerText = `© ${new Date().getFullYear()}. Built with ${buildWith}.`;
-
-//About
-export const aboutText = [
-  "Digital craftsman specializing in elegant web solutions.",
-  "Committed to creating streamlined, efficient, and intuitive applications with meticulous attention to details.",
-];
 
 //Navigation
 export const navigationItems = [
